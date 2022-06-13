1066de671354c14bf8392eaab3ab6e7559dee8e7
Bijoy99roy <bijoy2014amp@gmail.com>
2022-06-14 01:48:38 +0530

    method for saving feedback implemented

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 from preprocessing.data_preprocessing import PreProcessing
 from prediction.prediction import Predictor
-
+from database.database import DataBase
+from datetime import datetime
 
 app = Flask(__name__)
+db_operator = DataBase()
+prediction_result = None
+tweet_value = None
 
 
 @app.route('/', methods=['GET'])
 def home():
     try:
+        if db_operator.is_connected():
+            db_operator.create_tables()
+        else:
+            db_operator.connect_db()
+            db_operator.create_tables()
+
         return render_template('index.html')
     except Exception as e:
         message = 'Error :: ' + str(e)
@@ -18,19 +28,46 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
+        global tweet_value, prediction_result
         if request.method == 'POST':
             tweet = str(request.form.get('tweet'))
+            tweet_value = tweet
         preprocessing_obj = PreProcessing()
         tweet_preprocessed = preprocessing_obj.remove_noises(tweet)
         tweet_preprocessed = preprocessing_obj.join_texts(tweet_preprocessed)
         tweet_vectorized = preprocessing_obj.vectorize_data(tweet_preprocessed)
         predictor = Predictor()
         prediction = predictor.predict(tweet_vectorized)[0]
+        prediction_result = prediction
         if prediction == 1:
             sentiment = 'Positive Sentiment'
         else:
             sentiment = 'Negative Sentiment'
         return render_template('result.html', data={'result': sentiment})
+    except Exception as e:
+        message = 'Error :: ' + str(e)
+        return render_template('exception.html', exception=message)
+
+
+@app.route('/feedback', methods=['POST'])
+def save_feedbacks():
+    try:
+
+        if request.method == 'POST':
+            feedback = request.form['feedback']
+        now = datetime.now()
+        date = now.date()
+        current_time = now.strftime('%H:%M:%S')
+        if prediction_result == 1 and feedback == 'Yes':
+            sentiment = 'Positive'
+        elif prediction_result == 1 and feedback == 'No':
+            sentiment = 'Negative'
+        elif prediction_result == 0 and feedback == 'Yes':
+            sentiment = 'Negative'
+        else:
+            sentiment = 'Positive'
+        db_operator.insert_data('dataset', date, current_time, tweet_value, sentiment)
+        return redirect(url_for('home'))
     except Exception as e:
         message = 'Error :: ' + str(e)
         return render_template('exception.html', exception=message)
